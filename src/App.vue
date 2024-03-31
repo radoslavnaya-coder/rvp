f1d3c44765173d77dee111f3b2ee760c007027dd
radoslavnaya-coder <killaarte13@gmail.com>
2024-03-31 20:00:10 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TheHeader from './components/Header/TheHeader.vue'
+import TheBanner from './components/Header/TheBanner.vue'
+import TheCounter from './components/Main/TheCounter.vue'
+import TheProjects from './components/Main/TheProjects.vue'
+import AboutCompany from './components/Main/AboutCompany.vue'
+import FinalWork from './components/Main/FinalWork.vue'
+</script>
+
+<template>
+  <TheHeader />
+  <TheBanner />
+  <TheCounter />
+  <TheProjects />
+  <AboutCompany />
+  <FinalWork />
+</template>
+
+<style>
+@import url('@/assets/main.css');
+</style>
